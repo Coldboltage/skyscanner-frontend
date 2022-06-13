005940d32ca48bf14324017ee22b5996000bcc9c
Coldboltage <alanreid@hotmail.co.uk>
2022-06-13 11:15:56 +0100

    added this for penta

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
