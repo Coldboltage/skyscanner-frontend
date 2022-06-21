4d1db486904a6dc22b1f96e963606d422bfda212
Coldboltage <alanreid@hotmail.co.uk>
2022-06-21 12:48:44 +0100

    fixed mobile width

--- components/CheapestFlightsItem.js
+++ components/CheapestFlightsItem.js
@@ -2,30 +2,37 @@ import React from "react";
 import * as styles from "../styles/CheapestFlightsItem.module.css";
 
 const cheapestFlightsItem = ({ flight, bestOrCheapest }) => {
-  const {cost, time, arrival, durationOfFlight} = flight[bestOrCheapest]
-  const {flightDatesString: {departDate, returnDate}, url} = flight
+  const { cost, time, arrival, durationOfFlight } = flight[bestOrCheapest];
+  const {
+    flightDatesString: { departDate, returnDate },
+    url,
+  } = flight;
   return (
     <div className={styles.inputForm}>
-      <p className={styles.individualPara}>
-        <span className={styles.titleItem}>Cost</span>:
-         £{cost}
-      </p>
-      <p className={styles.individualPara}>
-        <span className={styles.titleItem}>Dates</span>:
-        {` `}{departDate} - {returnDate}
-      </p>
-      <p className={styles.individualPara}>
+      <div className={styles.individualPara}>
+        <span className={styles.titleItem}>Cost</span>: £{cost}
+      </div>
+      <div className={styles.individualPara}>
+        <span className={styles.titleItem}>Dates</span>:{` `}
+        {departDate} - {returnDate}
+      </div>
+      <div className={styles.individualPara}>
         <span className={styles.titleItem}>Time</span>
-        {` `}{time} - {arrival}
-      </p>
-      <p className={styles.individualPara}>
+        {` `}
+        {time} - {arrival}
+      </div>
+      <div className={styles.individualPara}>
         <span className={styles.titleItem}>Duration</span>
-        {` `}{durationOfFlight} 
-      </p>
-      <a href={url} className={`${styles.individualPara} ${styles.link}`}>
-        <span className={styles.titleItem}>Skyscanner Link</span>
-        {` `}<a href={url}>Flight Link</a>
-      </a>
+        {` `}
+        {durationOfFlight}
+      </div>
+      <div className={styles.individualPara}>
+        <a href={url} className={`${styles.individualPara} ${styles.link}`}>
+          <span className={styles.titleItem}>Skyscanner Link</span>
+          {` `}
+          <a href={url}>Flight Link</a>
+        </a>
+      </div>
     </div>
   );
 };
