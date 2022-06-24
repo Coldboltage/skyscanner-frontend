141bf1d18bda398b8fa7454d8df681982a1ab758
Coldboltage <alanreid@hotmail.co.uk>
2022-06-24 18:14:04 +0100

    added extract env for localhost dev

--- components/ReferenceList.js
+++ components/ReferenceList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import * as styles from "../styles/CheapestFlightsItem.module.css";
-import Link from "next/link"
+// import Link from "next/link"
 
 const ReferenceItem = ({ reference }) => {
+  const localURL = process.env.NEXT_PUBLIC_LOCALHOST_DEV
   return (
     <div className={styles.inputForm}>
       <div className={styles.individualPara}>
-        <Link href={`${process.env.NEXT_PUBLIC_LOCALHOST||""}/ref?ref=${reference} `}>{reference}</Link>
+        <a href={`${localURL||""}/ref?ref=${reference}`} target="_blank" rel="noopener noreferrer">{reference}</a>
       </div>
     </div>
   );
